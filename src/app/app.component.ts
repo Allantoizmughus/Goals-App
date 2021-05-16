595b52300dcd8b9e176c474ee68dd373ebd56a55
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-05-16 12:37:25 +0300

    Create an angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Angular-app1';
-}
+ @Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+ })
+
+
+export class AppComponent{
+  
+
+} 
+
+
